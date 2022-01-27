2e999f2b48a4d81c5afcf37ae2ad52625d47da1a
Axel Raboit <axel.raboit@gmail.com>
2022-01-27 16:04:00 +0100

    training about refs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
+import React, { Component } from 'react';
+import MyRef from './components/MyRef';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
 
-    </div>
-  );
+  constructor(props) {
+    super(props)
+  
+    this.refComp = React.createRef();
+  }
+
+  handleClick = () => {
+    /* console.log(this.refComp.current); */
+    this.refComp.current.addFocus();
+  }
+  
+
+  render() {
+    return (
+      <div className="App">
+        <MyRef ref={this.refComp}/>
+        <button onClick={this.handleClick}>Valider</button>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/MyRef.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class MyRef extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            value: ''
+        }
+
+        this.myTitle = React.createRef();
+        this.myInput = React.createRef();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        this.myTitle.current.style.color = "red";
+    }
+
+    addFocus = () => {
+        this.myInput.current.focus();
+    }
+    
+    render() {
+        return (
+            <div>
+                <input ref={this.myInput} type="text" />
+            </div>
+        )
+    }
+}
+
+export default MyRef;
